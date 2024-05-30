477ea86bbcd2b420ae8f543e5beb145bec20d8a8
Joel Montilla <joel.montillaheredia@plexus.es>
2024-05-30 15:42:27 +0200

    listado y buscador

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router'; // Importa el Router
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -6,5 +11,26 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SearchPageComponent {
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
 
+  constructor(private heroesService: HeroesService, private router: Router) { } // Injecta el Router
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+    this.heroesService.getSuggestions(value)
+      .subscribe(heroes => {
+        this.heroes = heroes.filter(hero =>
+          hero.superhero.toLowerCase().includes(value.toLowerCase()));
+      });
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    const heroName: string = event.option.value;
+    const hero = this.heroes.find(h => h.superhero === heroName);
+    if (hero) {
+
+      this.router.navigate(['/heroes', hero.id]);
+    }
+  }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,24 +1,27 @@
 import { environments } from './../../../environments/environments';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 
-
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
+  private baseUrl: string = environments.baseUrl;
 
-  private baseUrl:string = environments.baseUrl;
   constructor(private http: HttpClient) { }
 
-
-
-  getHeroes():Observable<Hero[]>{
+  getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
-
-
   }
 
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(error => of(undefined))
+      );
+  }
+
+  getSuggestions(query: string): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
 }
-
-

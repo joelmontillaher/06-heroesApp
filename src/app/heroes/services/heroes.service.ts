7bf7cc3e09c1fd3940593f04f6a1340fbda147a8
Joel Montilla <joel.montillaheredia@plexus.es>
2024-05-31 10:14:50 +0200

    crear, editar y eliminar

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { environments } from './../../../environments/environments';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, of, map	 } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -23,5 +23,22 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero(hero: Hero):Observable<Hero>{
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+  updateHero(hero: Hero):Observable<Hero>{
+    if (!hero.id) throw Error('Hero id is required')
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+  deleteHeroById(id: string):Observable<boolean>{
+
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError( err => of(false)),
+      map(resp => true)
+    )
+    ;
   }
 }
